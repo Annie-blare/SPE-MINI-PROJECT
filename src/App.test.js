951e598790afea1d9a3e3e5e8f104bd84cb78ac1
Annie-blare <109508402+Annie-blare@users.noreply.github.com>
2024-02-16 16:22:46 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Calculator App', () => {
+  test('renders calculator input field', () => {
+    render(<App />);
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+
+  // test('allows numbers to be inputted', () => {
+  //   render(<App />);
+  //   const inputField = screen.getByRole('textbox');
+  //   userEvent.click(screen.getByText('1'));
+  //   expect(inputField.value).toBe('1');
+  // });
+
+  // test('can perform addition', () => {
+  //   render(<App />);
+  //   userEvent.click(screen.getByText('2'));
+  //   userEvent.click(screen.getByText('+'));
+  //   userEvent.click(screen.getByText('3'));
+  //   userEvent.click(screen.getByText('='));
+  //   expect(screen.getByRole('textbox').value).toBe('5');
+  // });
+
+  // test('can perform subtraction', () => {
+  //   render(<App />);
+  //   userEvent.click(screen.getByText('5'));
+  //   userEvent.click(screen.getByText('-'));
+  //   userEvent.click(screen.getByText('2'));
+  //   userEvent.click(screen.getByText('='));
+  //   expect(screen.getByRole('textbox').value).toBe('3');
+  // });
+
+  // test('can clear input', () => {
+  //   render(<App />);
+  //   userEvent.click(screen.getByText('9'));
+  //   userEvent.click(screen.getByText('Clear'));
+  //   expect(screen.getByRole('textbox').value).toBe('');
+  // });
+
+  // test('handles division by zero with error', () => {
+  //   render(<App />);
+  //   userEvent.click(screen.getByText('1'));
+  //   userEvent.click(screen.getByText('/'));
+  //   userEvent.click(screen.getByText('0'));
+  //   userEvent.click(screen.getByText('='));
+  //   expect(screen.getByRole('textbox').value).toBe('Error');
+  // });
+
+  
 });
